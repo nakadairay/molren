29f82286f93035d324b90b104743839eb454067d
中平 慶春 <nakadaira@nakahiranoMacBook-Pro-2.local>
2021-08-20 11:42:45 +0900

    クレジットカード登録の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   get 'users/show'
   get 'practices/index'
   devise_for :users
   root "practices#index"
 
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_020907) do
+ActiveRecord::Schema.define(version: 2021_08_20_023625) do
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "customer_token", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
 
   create_table "practices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -39,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_08_20_020907) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
 end
