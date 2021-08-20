6910de8717bdf5b93b322fbd5c21ea140e88ef17
中平 慶春 <nakadaira@nakahiranoMacBook-Pro-2.local>
2021-08-20 11:13:11 +0900

    practice モデルコントローラーのジェネレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_012617) do
+ActiveRecord::Schema.define(version: 2021_08_20_020907) do
+
+  create_table "practices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Practice.create(name: '練習会１', price: 500)
+Practice.create(name: '練習会２', price: 1000)
+Practice.create(name: '練習会３', price: 800)
+Practice.create(name: '練習会４', price: 1200)
+Practice.create(name: '練習会５', price: 300)
+Practice.create(name: '体験会１', price: 100)
+Practice.create(name: '体験会２', price: 300)
+Practice.create(name: '体験会３', price: 2000)
