d031ff6c71df11964f6bac0c30bb977c9e29ce05
中平 慶春 <nakadaira@nakahiranoMacBook-Pro-2.local>
2021-08-20 20:21:49 +0900

    マイページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
   
   def show
+    @user = User.find(params[:id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     card = Card.find_by(user_id: current_user.id)
 
